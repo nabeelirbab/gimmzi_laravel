b84f65f50785b0590055ee8f517503e0fdf4ea5a
unknown <ehsankhan0577@gmail.com>
2025-04-11 01:13:51 +0500

    explore and gimmzi universe route added

--- resources/views/frontend/business/website.blade.php
+++ resources/views/frontend/business/website.blade.php
@@ -436,8 +436,9 @@
                         </div> --}}
                     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Explore</a></li>
-                            <li class="breadcrumb-item"><a href="#">Gimmzi Market Universe</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('frontend.explore') }}">Explore</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('frontend.market-universe') }}">Gimmzi Market
+                                    Universe</a></li>
                             <li class="breadcrumb-item active" aria-current="page">
                                 {{ $business->category->category_name }}</li>
                         </ol>
